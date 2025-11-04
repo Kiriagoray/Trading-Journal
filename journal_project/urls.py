451675e409e583b4c54058565344920061591da7
kiriagoray <kiriagoraymond@gmail.com>
2025-11-05 01:24:25 +0300

    Add root-level favicon route - browsers request /favicon.ico directly

--- journal_project/urls.py
+++ journal_project/urls.py
@@ -5,10 +5,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('journal.urls')),
+    # Serve favicon.ico at root URL - browsers automatically request /favicon.ico
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')), name='favicon'),
 ]
 
 # Serve media files in development
