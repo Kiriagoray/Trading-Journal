1f33d3023f0cabb67c0fca75b6b1c8f4304b5b9e
kiriagoray <kiriagoraymond@gmail.com>
2025-11-05 01:25:17 +0300

    Fix favicon route to use direct static path instead of storage.url

--- journal_project/urls.py
+++ journal_project/urls.py
@@ -6,13 +6,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
-from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('journal.urls')),
     # Serve favicon.ico at root URL - browsers automatically request /favicon.ico
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')), name='favicon'),
+    # Redirect to the static file location served by WhiteNoise
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True), name='favicon'),
 ]
 
 # Serve media files in development
